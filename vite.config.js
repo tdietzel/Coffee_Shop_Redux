d8eff2830de9ffd4216b2a3dcdde26953adb2276
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-12-16 21:31:44 -0800

    Update gh-pages location

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Coffee_Shop_Inventory/"
+  base: "/Coffee_Shop_Redux/"
 })
